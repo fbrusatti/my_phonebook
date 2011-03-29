bb673d1174bde414a5876f05cf39f361037a4eba
Franco Brusatti <fbrusatti@gmail.com>
2011-03-29 04:53:59 -0300

    Added Validations and Formats on email and phone attributes to Contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,17 @@
 class Contact < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :email, :phone, :avatar
 
+  # == Validations
+  validates :first_name, :presence => true
+
+  validates :email,
+            :presence => true,   
+            :uniqueness => true,   
+            :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }
+
+  validates :phone, :format => {:with => /^00[\s]\d{3}[\s]\d{6}$/}
+
+
   has_attached_file :avatar,
     :styles => { :thumb => "100x100>" },
     :default_url => "/images/dummy_100x100.png",
